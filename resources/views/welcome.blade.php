9ca8c083054493e8ccaea54f01277a2d2e97df61
nicholas-yangding <dingni666666@163.com>
2017-06-07 19:40:20 +0800

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Just For Fun!</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+        </style>
+    </head>
+    <body>
+        
+        <div class="container" style="width: 100%; margin-left: -500px">
+            <div class="content" >
+                <div class="title">Hi! Nicholas Wang</div>
+                <div class="title">baby come &lt;on!</div>
+                <div style="font-family: serif;font-size: 60px">Gorgeous</div>
+                <form >
+                <input type="text" name="var1"/>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
